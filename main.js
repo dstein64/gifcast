d152fc7137754caf53afd399947fe5e559d7015d
Henrik Hellerstedt <henrik.hellerstedt@pharmetheus.com>
2021-10-27 09:15:57 +0200

    Added gruvbox-light and gruvbox-dark themes.

--- main.js
+++ main.js
@@ -200,6 +200,22 @@ const THEMES = {
          background: '#002451',   foreground: '#ffffff',      cursor: '#ffffff', cursorAccent: '#002451',
           selection: '#003f8e',
     },
+    gruvbox_dark: {
+              black: '#282828',          red: '#cc241d',       green: '#98971a',       yellow: '#d79921',
+               blue: '#458588',       purple: '#b16286',        cyan: '#689d6a',        white: '#a89984',
+        brightBlack: '#928374',    brightRed: '#fb4934', brightGreen: '#b8bb26', brightYellow: '#fabd2f',
+         brightBlue: '#83a598', brightPurple: '#d3869b',  brightCyan: '#8ec07c',  brightWhite: '#ebdbb2',
+         background: '#282828',   foreground: '#ebdbb2',      cursor: '#ebdbb2', cursorAccent: '#002451',
+          selection: '#003f8e',
+    },
+    gruvbox_light: {
+              black: '#fbf1c7',          red: '#cc241d',       green: '#98971a',       yellow: '#d79921',
+               blue: '#458588',       purple: '#b16286',        cyan: '#689d6a',        white: '#a89984',
+        brightBlack: '#928374',    brightRed: '#fb4934', brightGreen: '#b8bb26', brightYellow: '#fabd2f',
+         brightBlue: '#83a598', brightPurple: '#d3869b',  brightCyan: '#8ec07c',  brightWhite: '#ebdbb2',
+         background: '#fbf1c7',   foreground: '#3c3836',      cursor: '#002451', cursorAccent: '#ebdbb2',
+          selection: '#a89984',
+    },
 };
 
 const FONT_CANDIDATES = function() {
